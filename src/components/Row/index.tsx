df92c97df2fd67d46c5bf1b81bf0de95b6cda4d6
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-22 21:06:12 -0300

    building style of movies

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import { getMovies } from '../../lib/api'
 import { Movie } from '../../@types/moviesType'
+import './style.css'
+
+
 
 const imageHost = 'https://image.tmdb.org/t/p/original/'
 export const Row = ({title, path}:{title:string, path:string}) => {
@@ -26,7 +29,12 @@ export const Row = ({title, path}:{title:string, path:string}) => {
       <div className='row-cards'>
         {movies.map((movie:Movie) => {
           return (
-            <img key={movie.id} src={imageHost+movie.poster_path} alt={movie.name}/>
+            <img 
+              key={movie.id}
+              src={imageHost+movie.poster_path}
+              alt={movie.name}
+              className='movie-card'
+            />
           )
         })}
       </div>
