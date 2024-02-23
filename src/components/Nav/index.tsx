b26a8f8d04ad0a8042d4552986f7ca31c8d2492c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-23 19:40:21 -0300

    adding transiction to header

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 
 
 export const Nav = () => {
-  return (
-    <div className='nav-container'>
-        <div className="nav-logo"></div>
-        <img className="nav-avatar" alt='logo'/>
-    </div>
-  )
+    const [blackHeader, setBlackHeader] = useState(false)
+
+    useEffect(() => {
+        addEventListener('scroll', () => {
+            scrollY>100 && setBlackHeader(true)
+            scrollY<100 && setBlackHeader(false)
+        })
+
+    },[])
+
+    return (
+        <div className={`nav-container ${blackHeader &&'nav-container-black'}`}>
+            <img className="nav-logo" alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'/>
+            <img className="nav-avatar" alt='avatar' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU'/>
+        </div>
+    )
 }
