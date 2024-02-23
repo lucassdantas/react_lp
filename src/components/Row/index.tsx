243ae6dc2dd9c6bfb4d29d09dcf411e66b8eb553
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-02-22 21:00:35 -0300

    fetching api working and ussing imags

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import { categoriesType } from '../../@types/categoriesType'
+import { useEffect, useState } from 'react'
+import { getMovies } from '../../lib/api'
+import { Movie } from '../../@types/moviesType'
 
+const imageHost = 'https://image.tmdb.org/t/p/original/'
+export const Row = ({title, path}:{title:string, path:string}) => {
+  const [movies, setMovies] = useState([])
+  
+  const fetchMovies = async (_path:string) => {
+    try {
+      const data = await getMovies(_path)
+      setMovies(data.results)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-
-export const Row = ({name, title, path}:{name:string, title:string, path:string}) => {
+  useEffect(() => {
+    fetchMovies(path)
+  }, [path])
+  
   return (
-    <>
-    {name} {title} {path}
-    <div>index</div>
-    </>
+    <div className='row-container'>
+      <h2 className='row-header'>{title}</h2>
+      <div className='row-cards'>
+        {movies.map((movie:Movie) => {
+          return (
+            <img key={movie.id} src={imageHost+movie.poster_path} alt={movie.name}/>
+          )
+        })}
+      </div>
+    </div>
   )
 }
